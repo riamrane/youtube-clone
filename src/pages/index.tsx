35f991f4656954eb73b10953f3ba27b5a2e0f740
ria <riamrane@gmail.com>
2025-07-12 23:32:23 +0530

    search bar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+import AppBar from "@/components/AppBar";
 import { VideoGrid } from "@/components/VideoGrid";
 
 
 export default function Home() {
   return (
     <div>
+      <AppBar/>
       <VideoGrid/>
     </div>
   );
